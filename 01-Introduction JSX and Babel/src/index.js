7e1eb1128a4fb23c1999ef505a56dd0dcfa8ffa0
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-07-28 17:27:22 +0300

    edited

---
+++ 01-Introduction JSX and Babel/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>this is react</p>
+  </div>,
+
+  document.getElementById("root")
+);
