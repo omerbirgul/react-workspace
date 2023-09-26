c7472b284de10ae210c286b2bf809cc139e9c0ef
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-26 22:56:06 +0300

    map data practice emojiPedia

---
+++ 17-Mappind Data to Components Practice (EmojiPedia)/src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEmoji(emoji) {
+  return (
+    <Entry
+      key={emoji.id}
+      id={emoji.id}
+      emoji={emoji.emoji}
+      name={emoji.name}
+      description={emoji.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
+    </div>
+  );
+}
+
+export default App;
