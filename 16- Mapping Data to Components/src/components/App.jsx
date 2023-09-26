0be46b66d596c3073f49368f80ed25debb3245fa
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-26 21:53:26 +0300

    react mapping function 2

--- 16- Mapping Data to Components/src/components/App.jsx
+++ 16- Mapping Data to Components/src/components/App.jsx
@@ -5,11 +5,13 @@ import contacts from "../contacts";
 function createCard(contact){
   return <Card
   key={contact.id}
+  id= {contact.id}
   name={contact.name}
   img={contact.imgURL}
   tel= {contact.phone}
   email={contact.email} />
 }
+
 
 function App() {
   return (
