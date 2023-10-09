965c31c64cb1baba9397fe22ab1b62951d6da95b
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-09 21:54:50 +0300

    find method

--- 18-Javascript ES6 Functions/src/index.js
+++ 18-Javascript ES6 Functions/src/index.js
@@ -13,11 +13,15 @@ var numbers = [3, 56, 2, 48, 5];
 // console.log(newNumbers);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-const newNumber = numbers.reduce(function(accumulator, currentNumber){
-  return accumulator + currentNumber;
-});
-console.log(newNumber);
+// const newNumber = numbers.reduce(function(accumulator, currentNumber){
+  // return accumulator + currentNumber;
+// });
+// console.log(newNumber);
 
 //Find - find the first item that matches from an array.
+const newNumber = numbers.find(function(x){
+  return x < 3;
+})
+console.log(newNumber);
 
 //FindIndex - find the index of the first item that matches.
