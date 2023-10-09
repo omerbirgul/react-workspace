074dc569fb7141e166cc3a2b8f2e5b8ed604ff76
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-09 21:45:40 +0300

    Map function

--- 18-Javascript ES6 Functions/src/index.js
+++ 18-Javascript ES6 Functions/src/index.js
@@ -1,6 +1,10 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+const newNumbers = numbers.map(function(x){
+  return x * 2;
+});
+console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
 
