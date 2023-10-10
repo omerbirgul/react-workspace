799a1ba1f00c290274b9045953d6da85014d8623
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-11 00:43:49 +0300

    conditioanl rendering best solution

--- 22-Conditional Rendering Exercise/src/components/App.jsx
+++ 22-Conditional Rendering Exercise/src/components/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Form from "./Form";
-import Login from "./Login";
 
-var userIsRegistered = true;
-
+var userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
-      {userIsRegistered === false ? <Form /> : <Login />}
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
--- 22-Conditional Rendering Exercise/src/components/Form.jsx
+++ 22-Conditional Rendering Exercise/src/components/Form.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Input from "./Input";
 
-function Form() {
+function Form(props) {
   return (
     <form className="form">
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
-      <Input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {!props.isRegistered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
     </form>
   );
 }
