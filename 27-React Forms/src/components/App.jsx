a9fe90347d157e3a833ca165d196c4518763e8ee
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-13 23:43:10 +0300

    react forms setName version1

---
+++ 27-React Forms/src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {name} </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+      />
+      <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
